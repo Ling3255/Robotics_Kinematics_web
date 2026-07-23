4113b4d904c4b1671d2f64b39c72ca47e093412f
Ling3255 <Eva.Ling27683255@proton.me>
2026-07-23 20:48:15 +0800

    修复 robot-basics 第三页视口高度不居中问题

--- src/app/robot-basics/FbxViewer.tsx
+++ src/app/robot-basics/FbxViewer.tsx
@@ -546,7 +546,7 @@ export default function FbxViewer() {
   const controlsRef = useRef<OrbitControlsImpl | null>(null);
 
   return (
-    <div className="relative h-full w-full">
+    <div className="relative h-[calc(100vh-112px)] w-full">
       <div className="flex h-full w-full">
         <div className="relative h-full w-1/2">
           <Canvas>
